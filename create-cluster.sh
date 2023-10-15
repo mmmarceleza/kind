0a5e2ab2e278d5314a4b79e79a371243578e72fe
mmmarceleza <58913502+mmmarceleza@users.noreply.github.com>
2023-10-15 09:24:53 -0300

    chore: commit save point

--- create-cluster.sh
+++ create-cluster.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # create the cluster
-kind create cluster --image kindest/node:v1.26.6
+kind create cluster --image kindest/node:v1.26.6 --config config.yaml
 
 # install metrics-server (https://github.com/kubernetes-sigs/metrics-server)
-helm repo add metrics-server https://kubernetes-sigs.github.io/metrics-server/
-helm repo update
-helm upgrade --install metrics-server metrics-server/metrics-server -n metrics-server --create-namespace \
---version 3.10.0 \
+helm upgrade --install metrics-server metrics-server -n metrics-server --create-namespace \
+--repo https://kubernetes-sigs.github.io/metrics-server/ \
+--version 3.11.0 \
 --set args={--kubelet-insecure-tls} \
 --wait
+
